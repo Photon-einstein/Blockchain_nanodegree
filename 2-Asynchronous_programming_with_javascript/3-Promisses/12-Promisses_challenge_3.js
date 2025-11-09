9ca07bb5416b9dbb9a2a03e6cbf60452240ec76a
Tiago Sousa <t.l.sousa@gmail.com>
2025-11-09 19:41:50 +0000

    Add exercises of promisses

---
+++ 2-Asynchronous_programming_with_javascript/3-Promisses/12-Promisses_challenge_3.js
@@ -0,0 +1,69 @@
+function randomNum(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+function randomReturn(resolve, reject) {
+  return Math.random() * 10 > 5 ? resolve : reject;
+}
+
+const p1 = new Promise((resolve, reject) => {
+  setTimeout(randomReturn(resolve, reject), randomNum(500, 4000), "A");
+});
+
+const p2 = new Promise((resolve, reject) => {
+  setTimeout(randomReturn(resolve, reject), randomNum(500, 4000), "A");
+});
+
+const p3 = new Promise((resolve, reject) => {
+  setTimeout(randomReturn(resolve, reject), randomNum(500, 4000), "A");
+});
+
+const p4 = new Promise((resolve, reject) => {
+  setTimeout(randomReturn(resolve, reject), randomNum(500, 4000), "A");
+});
+
+// Above are a bunch of Promises that will either resolve or reject randomly, and fulfill
+// in a random amount of time between half a second and 4 seconds.
+
+// Challenge 1
+// Implement the method to get a list of results of all the promises and console log how many failed and how many accepted
+Promise.allSettled([p1, p2, p3, p4])
+  .then((results) => {
+    let accepted = 0;
+    let failed = 0;
+
+    console.log("Challenge 1:");
+    console.log("\nResults: ", results, "\n");
+
+    results.forEach((result) => {
+      if (result.status == "fulfilled") {
+        ++accepted;
+      } else {
+        ++failed;
+      }
+    });
+    console.log("Accepted: ", accepted);
+    console.log("Failed: ", failed);
+  })
+  .catch((error) => {
+    console.log("Challenge 1:", error);
+  });
+
+// Challenge 2
+// Implement the method to get a list of results from all the Promises unless one fails. Log the list of successful results or the error message if one fails.
+Promise.all([p1, p2, p3, p4])
+  .then((results) => {
+    console.log("Challenge 2:");
+    console.log("\nResults: ", results, "\n");
+  })
+  .catch((error) => console.log("Challenge 2:", error));
+
+// Challenge 3
+// Implement the method to figure out which Promise fulfilled first, print the message it fulfills with.
+Promise.race([p1, p2, p3, p4])
+  .then((result) => {
+    console.log("Challenge 3:");
+    console.log(result);
+  })
+  .catch((error) => {
+    console.log("Challenge 3:", error);
+  });
